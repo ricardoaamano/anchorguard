77e8671a1db6378c3c67e5037119bae2624ff45c
ricardoaamano <ricardoaamano@outlook.com>
2024-10-23 00:20:24 -0600

    Actualización

--- scripts.js
+++ scripts.js
@@ -1,26 +1,6 @@
 const menuIcon = document.querySelector('.menu-icon');
 const navLinks = document.querySelector('.nav-links');
-let closeTimeout;
 
-function toggleMenu() {
+menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-}
-
-navLinks.addEventListener('mouseleave', () => {
-    closeTimeout = setTimeout(() => {
-        navLinks.classList.remove('active');
-    }, 300);
 });
-
-navLinks.addEventListener('mouseenter', () => {
-    clearTimeout(closeTimeout);
-});
-
-function closeMenu(event) {
-    if (!navLinks.contains(event.target) && !menuIcon.contains(event.target)) {
-        navLinks.classList.remove('active');
-    }
-}
-
-menuIcon.addEventListener('click', toggleMenu);
-document.addEventListener('click', closeMenu);
